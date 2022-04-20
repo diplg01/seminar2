eefbe73cf0c64ba10c7bfdeb058f3fb0411289a6
diplg <dimas4354@icloud.com>
2022-04-20 22:17:05 +0300

    second

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = new Random().Next(10, 99);
+Console.WriteLine(a);
+int b = a / 10;
+int c = a % 10;
+
+if (b > c)
+{
+    Console.WriteLine($"Наибольше число {b}");
+}
+else
+{
+    Console.WriteLine($"Наибольше число {c}");
+}
