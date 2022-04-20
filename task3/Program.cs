1b9840c15a87629bd5f88f96f263abfd7f788746
diplg <dimas4354@icloud.com>
2022-04-20 23:20:43 +0300

    III

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿//  кратность двух чисел и остаток от деления 
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a % b == 0)
+{
+    Console.WriteLine("кратно");
+}
+else 
+{
+    Console.WriteLine($"остаток от деления {a % b}");
+}
